f8377b59fb9f18a944a55f804b75ae9462f6742e
Johnny Oshika <johnnyoshika@gmail.com>
2020-08-24 11:59:40 -0700

    Add IAsyncEnumerable sample

--- AsyncEnumerable/Program.cs
+++ AsyncEnumerable/Program.cs
@@ -9,8 +9,24 @@ namespace ConsoleApp5
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-        }   
+            await foreach (var i in Test().WithCancellation(CancellationToken.None))
+                Console.WriteLine(i);
+
+            Console.WriteLine(await Test().Where(i => i > 2).FirstAsync());
+        }
+
+        static async IAsyncEnumerable<int> Test()
+        {
+            yield return 1;
+            yield return 2;
+
+            await Task.Delay(1000);
+
+            yield return 3;
+            yield return 4;
+        }
+            
     }
 }
